de277bf446ffb77d1568b779f576db1616b0cf41
jessicacorreaes <jessicacorreaes@gmail.com>
2022-01-30 19:37:54 -0300

    first commit

---
+++ CursoNelioAlves/src/main/java/com/educancoweb/CursoNelioAlves/CursoNelioAlvesApplication.java
@@ -0,0 +1,13 @@
+package com.educancoweb.CursoNelioAlves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioAlvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoNelioAlvesApplication.class, args);
+	}
+
+}
---
+++ CursoNelioAlves/src/test/java/com/educancoweb/CursoNelioAlves/CursoNelioAlvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educancoweb.CursoNelioAlves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNelioAlvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
