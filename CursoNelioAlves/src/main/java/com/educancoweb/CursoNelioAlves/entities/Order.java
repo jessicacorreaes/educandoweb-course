6e7c91b3bd1ad893ef366053a5597ffccde90968
jessicacorreaes <jessicacorreaes@gmail.com>
2022-01-31 01:04:11 -0300

    Payment, one-to-one association

--- CursoNelioAlves/src/main/java/com/educancoweb/CursoNelioAlves/entities/Order.java
+++ CursoNelioAlves/src/main/java/com/educancoweb/CursoNelioAlves/entities/Order.java
@@ -19,7 +19,7 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy=MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     private Integer orderStatus;
@@ -31,9 +31,14 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
-    public Order(){}
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order() {
+    }
 
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        super();
         this.id = id;
         this.moment = moment;
         setOrderStatus(orderStatus);
@@ -56,6 +61,16 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     public User getClient() {
         return client;
     }
@@ -64,18 +79,24 @@ public class Order implements Serializable {
         this.client = client;
     }
 
-    public OrderStatus getOrderStatus() {
-        return OrderStatus.valueOf(orderStatus);
+    public Payment getPayment() {
+        return payment;
     }
 
-    public void setOrderStatus(OrderStatus orderStatus) {
-        if(orderStatus != null) {
-            this.orderStatus = orderStatus.getCode();
-        }
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
-    public Set<OrderItem> getItems(){
+    public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItem x : items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
     }
 
     @Override
